7453e2f6bebe60d16f8f528e415d3b53e568ee7e
ilavv <ilias@lavidas.net>
2015-07-03 18:13:53 +0100

    one test only

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -6,7 +6,7 @@ echo "Running test suite"
 echo "Using $PERL5LIB"
 
 if [ "$COVERALLS" = 'true' ]; then
-  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t $SKIP_TESTS
+  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t/Array_ArrayChip.t $SKIP_TESTS
 else
   perl $PWD/ensembl-test/scripts/runtests.pl $PWD/modules/t $SKIP_TESTS
 fi
