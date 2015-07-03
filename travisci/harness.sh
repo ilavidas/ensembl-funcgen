842d7cf4a663e0056789261efb95ea8fef4ff3cc
ilavv <ilias@lavidas.net>
2015-07-03 17:58:04 +0100

    add test dir, expecting fails

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -2,4 +2,23 @@
 
 export PERL5LIB=$PWD/ensembl/ensembl-funcgen:$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl/modules:$PWD/ensembl-test/modules:$PWD/modules
 
-perl -v
+echo "Running test suite"
+echo "Using $PERL5LIB"
+
+if [ "$COVERALLS" = 'true' ]; then
+  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t $SKIP_TESTS
+else
+  perl $PWD/ensembl-test/scripts/runtests.pl $PWD/modules/t $SKIP_TESTS
+fi
+
+rt=$?
+
+if [ $rt -eq 0 ]; then
+  if [ "$COVERALLS" = 'true' ]; then
+    echo "Running Devel::Cover coveralls report"
+    cover --nosummary -report coveralls
+  fi
+  exit $?
+else
+  exit $rt
+fi
