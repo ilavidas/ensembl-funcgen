fb9713789f66e1d68d4fef9bb61ca97276e7e9d5
ilavv <ilias@lavidas.net>
2015-07-03 17:42:28 +0100

    modify harness.sh

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl/modules:$PWD/ensembl-test/modules:$PWD/modules
+export PERL5LIB=$PWD/ensembl/ensembl-funcgen:$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl/modules:$PWD/ensembl-test/modules:$PWD/modules
 
 perl -v
