34c0c17a52fc10576d00b5ef4d6fa739ec64f720
ilavv <ilias@lavidas.net>
2015-07-09 10:12:23 +0100

    trying to set it up properly

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-export PERL5LIB=$PWD/ensembl/ensembl-funcgen:$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl/modules:$PWD/ensembl-test/modules:$PWD/modules
+# export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl/modules:$PWD/ensembl-test/modules:$PWD/modules
+
+# perl -v
+
+export PERL5LIB=$PWD/ensembl-funcgen:$PWD/bioperl-live:$PWD/ensembl/modules:$PWD/ensembl-test/modules
 
 echo "Running test suite"
 echo "Using $PERL5LIB"
 
 if [ "$COVERALLS" = 'true' ]; then
-  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t/Array_ArrayChip.t $SKIP_TESTS
+  echo "IN"
+  PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/ensembl-funcgen/modules/t/ $SKIP_TESTS
 else
   perl $PWD/ensembl-test/scripts/runtests.pl $PWD/modules/t $SKIP_TESTS
 fi
